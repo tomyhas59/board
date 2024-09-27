da76abc8df91a5753be4b222fe9d8a5e978bf684
tomyhas59 <yh9035926@naver.com>
2024-09-27 09:48:56 +0900

    file upload

--- Java/board/src/main/java/com/example/board/controller/BoardController.java
+++ Java/board/src/main/java/com/example/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -13,61 +14,61 @@ public class BoardController {
 
     @Autowired
     private BoardService boardService;
+
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm() {
         return "boardWrite"; //boardWrite.html 문서 렌더링
     }
 
     @PostMapping("/board/writepro")
-    public  String boardWritePro(Board board,Model model){
+    public String boardWritePro(Board board, Model model, @RequestParam("file") MultipartFile file) throws Exception {
 
-          boardService.write(board);
+        boardService.write(board, file);
 
         model.addAttribute("message", "글 작성 완료");
         model.addAttribute("searchUrl", "/board/list");
 
-         return "message";
+        return "message";
     }
 
 
-
     @GetMapping("/board/list")
-    public String boardList(Model model){
+    public String boardList(Model model) {
 
-        model.addAttribute("list",boardService.boardList()); //html에 list라는 이름으로 전달
+        model.addAttribute("list", boardService.boardList()); //html에 list라는 이름으로 전달
 
         return "boardList";
     }
 
     @GetMapping("/board/view") //localhost:8080/board/view?id=1
-    public String boardView(Model model, @RequestParam("id") Integer id){
+    public String boardView(Model model, @RequestParam("id") Integer id) {
 
         model.addAttribute("board", boardService.boardView(id));
         return "boardView";
     }
 
     @GetMapping("/board/delete")
-    public String boardDelete(@RequestParam("id") Integer id){
+    public String boardDelete(@RequestParam("id") Integer id) {
 
-       boardService.boardDelete(id);
+        boardService.boardDelete(id);
         return "redirect:/board/list";
     }
 
     @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id, Model model){
-            model.addAttribute("board",boardService.boardView(id));
-            return "boardModify";
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardModify";
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board,Model model) {
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model, @RequestParam("file") MultipartFile file) throws Exception {
 
         Board existingBoard = boardService.boardView(id);
 
         existingBoard.setTitle(board.getTitle());
         existingBoard.setContent(board.getContent());
 
-        boardService.write(existingBoard);
+        boardService.write(existingBoard, file);
 
         model.addAttribute("message", "글 수정 완료");
         model.addAttribute("searchUrl", "/board/list");
--- Java/board/src/main/java/com/example/board/service/BoardService.java
+++ Java/board/src/main/java/com/example/board/service/BoardService.java
@@ -4,8 +4,12 @@ import com.example.board.entity.Board;
 import com.example.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -14,23 +18,36 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     //글 작성
-    public  void  write(Board board){
+    public void write(Board board, @RequestParam("file") MultipartFile file) throws Exception {
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String filename = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, filename);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(filename);
+        board.setFilepath("/files/" + filename);
+
         boardRepository.save(board);
     }
 
     //게시글 리스트 get
-    public List<Board> boardList(){
+    public List<Board> boardList() {
         return boardRepository.findAll();
     }
 
     //특정 게시글 불러오기
 
-    public Board boardView(Integer id){
-        return  boardRepository.findById(id).get();
+    public Board boardView(Integer id) {
+        return boardRepository.findById(id).get();
     }
 
     //게시글 삭제
-    public void boardDelete(Integer id){
+    public void boardDelete(Integer id) {
         boardRepository.deleteById(id);
     }
 }
